bfea68c0111afde8933710f8ef4de5566fad9947
ProPercivalalb <propercivalalb@gmail.com>
2013-08-24 20:05:52 +0100

    Main Class Created

---
+++ javacode/Main.java
@@ -0,0 +1,16 @@
+package javacode;
+
+/**
+ * @author Alex & Toby
+ * Main class that calls other classes to do there code
+ */
+public class Main {
+
+	/**
+	 * The first method called when the program is started
+	 * @param args Any arguments from cmd or terminal
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
